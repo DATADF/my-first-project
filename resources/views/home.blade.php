a6ba12d713e43f5b6446ed75a70941de3b59f58a
DATADF <klaytonfantin@outlook.com>
2019-07-11 16:50:49 -0300

    Init 12

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,5 +6,8 @@
 @section('content')
     <h1>Home Page</h1>
 
+    <h4>Iniciar aula 12</h4>
+    <a href="https://www.youtube.com/watch?v=xB85dtFtauE&list=PLpzy7FIRqpGD0kxI48v8QEVVZd744Phi4&index=13&t=0s">Aula 12</a>
+
 
 @endsection
