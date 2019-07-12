50380197b43f00731dc8b73a68065e2c2234d91c
DATADF <klaytonfantin@outlook.com>
2019-07-12 17:01:05 -0300

    Home aula 18

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,11 @@
 @section('content')
     <h1>Home Page</h1>
 
-    <h4>Iniciar aula 12</h4>
-    <a href="https://www.youtube.com/watch?v=xB85dtFtauE&list=PLpzy7FIRqpGD0kxI48v8QEVVZd744Phi4&index=13&t=0s">Aula 12</a>
+    <h4>Iniciar aula 19</h4>
+
+    <p>Laravel 5.8 Tutorial From Scratch - e18 - Eloquent Route Model Binding & RESTful Controller - Part 4</p>
+    <a href="https://www.youtube.com/watch?v=7n2ZElFqP1I&list=PLpzy7FIRqpGD0kxI48v8QEVVZd744Phi4&index=18">Aula 18</a>
+    <p>Verificar em 14 minutos a criação de Controller + Migration e Model</p>
 
 
 @endsection
